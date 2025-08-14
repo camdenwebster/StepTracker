68f6e5d90cce615b25218d7964f73205cd7ff6a3
Camden Webster <59626059+camdenwebster@users.noreply.github.com>
2025-08-13 20:46:23 -0500

    Initial Commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Camden Webster on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
