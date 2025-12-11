e44c10fd404a347ccd7c61ab0abcf5c9dc19ba5e
Camden Webster <59626059+camdenwebster@users.noreply.github.com>
2025-12-11 14:43:32 -0600

    Add step bar chart interactivity

--- StepTracker/Views/DashboardView.swift
+++ StepTracker/Views/DashboardView.swift
@@ -28,6 +28,7 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
+    @State private var rawSelectedDate: Date?
     
     var isSteps: Bool { selectedStat == .steps}
     
@@ -35,6 +36,13 @@ struct DashboardView: View {
         guard hkManager.stepData.isEmpty else { return 0 }
         let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
         return totalSteps/Double(hkManager.stepData.count)
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        return hkManager.stepData.first {
+            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
     }
     
     var body: some View {
@@ -68,6 +76,14 @@ struct DashboardView: View {
                         .padding(.bottom, 12)
                         
                         Chart {
+                            if let selectedHealthMetric {
+                                RuleMark(x: .value("Selected Metric", selectedHealthMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .annotation(position: .top,
+                                                spacing: 0,
+                                                overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) { annotationView }
+                            }
                             RuleMark(y: .value("Average", avgStepCount))
                                 .foregroundStyle(Color.secondary)
                                 .lineStyle(.init(lineWidth: 1, dash: [5]))
@@ -77,9 +93,11 @@ struct DashboardView: View {
                                     y: .value("Steps", steps.value)
                                 )
                                 .foregroundStyle(Color.pink.gradient)
+                                .opacity(rawSelectedDate == nil  || steps.date == selectedHealthMetric?.date ? 1.0 : 0.3)
                             }
                         }
                         .frame(height: 150)
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis {
                             AxisMarks { value in
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -119,7 +137,7 @@ struct DashboardView: View {
             .padding()
             .task {
                 // Uncomment to add mock data at launch
-//                await hkManager.addSimulatorData()
+                await hkManager.addSimulatorData()
                 await hkManager.fetchStepCount()
                 isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
             }
@@ -134,6 +152,23 @@ struct DashboardView: View {
             })
         }
         .tint(isSteps ? .pink : .indigo)
+    }
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+                .font(.footnote.bold())
+                .foregroundStyle(.secondary)
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
+        }
+        .padding(12)
+        .background(RoundedRectangle(cornerRadius: 4)
+            .fill(Color(.secondarySystemBackground))
+            .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        )
+        
     }
 }
 
